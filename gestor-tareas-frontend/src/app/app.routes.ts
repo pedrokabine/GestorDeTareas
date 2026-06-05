b6d24d1fbaa2094123ccd3c5b380355ca717e46f
Pedro Kabine <keita.pedro.smr@gmail.com>
2026-06-05 13:57:32 +0200

    feat: proteger ruta de tareas en Angular

--- gestor-tareas-frontend/src/app/app.routes.ts
+++ gestor-tareas-frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/pages/login/login.component';
 import { RegistroComponent } from './features/auth/pages/registro/registro.component';
 import { ListadoTareasComponent } from './features/tareas/pages/listado-tareas/listado-tareas.component';
+import { authGuard } from './features/auth/guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -12,10 +13,11 @@ export const routes: Routes = [
     path: 'registro',
     component: RegistroComponent
   },
-  {
-    path: 'tareas',
-    component: ListadoTareasComponent
-  },
+ {
+  path: 'tareas',
+  component: ListadoTareasComponent,
+  canActivate: [authGuard]
+},
   {
     path: '',
     redirectTo: 'login',
